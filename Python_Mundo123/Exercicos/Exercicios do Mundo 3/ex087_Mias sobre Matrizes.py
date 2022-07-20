8d81740b665dae591b1eea327784ab32eaa34560
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-07-20 22:29:57 +0200

    Update ex087_Mias sobre Matrizes.py

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex087_Mias sobre Matrizes.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex087_Mias sobre Matrizes.py
@@ -24,4 +24,7 @@ print(f' A soma dos valores da 3 coluna é {scol}')
 
 for c in range(0, 3):
     if c == 0:
-        mai = matriz
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior foi {mai}')
