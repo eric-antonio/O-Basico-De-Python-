11d2fe0251e88cb011efb519caeb28e2f8f7e72d
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-07-28 04:28:06 +0200

    Alteração No ficheiro dicas

---
+++ Python_Mundo123/Aulas/aula019_Dicionarios/Dicionario.py
@@ -0,0 +1,35 @@
+"""" Para instanciar dados em Python é necessario Ter em conrta os fundamentos anteriores abordados.
+Ex:
+ dados = dict()- é uma das formas de instanciar dados em Python.
+ dados = { 'nome':'Pedro' , 'idade':25}
+o indice agora são': nome e idade!
+ Temos agora o seguinte Dicionario:
+     -------------
+     'Pedro'| 25 |
+     ------------
+     Para adicioanar um novo indice juntamente com um elemento faz-se uso do comando:
+     dados[sexo] = 'M'
+     Passando o nosso dicionario a ser composto por:
+        nome  idade  sexo
+      --------------------
+     |'Pedro'| 25 |   'M'|
+     --------------------
+     Para eleiminar um indice juntamente com os seu elemento faz-se uso do comando:
+     del dados['idade']
+     Passando o nosso dicionario a ser composto por:
+         nome   sexo
+      --------------
+     |'Pedro'| 'M'|
+     ---------------
+
+     Os dicionarios são referenciados por 3 termos as 'KYES' 'VALUES' 'ITEMS'
+     Uma representação ilustrariva na fig 1.jgp
+
+
+     E Possivel adicionar um diconario a uma lista locadora = list()
+     Uma representação ilustrariva na fig 2.jgp
+
+
+     Caso exista alguma duvida recomendo rever o video!
+     https://youtu.be/ZWj8o692qGY
+"""
