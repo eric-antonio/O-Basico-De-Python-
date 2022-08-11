f8625e7e29d2c2a108205839ae70ccdabcf75caa
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-11 13:12:02 +0200

    Update ex095_Aprimorando os Dicionarios.py

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex095_Aprimorando os Dicionarios.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex095_Aprimorando os Dicionarios.py
@@ -19,21 +19,27 @@ while True:
         print('\033[0;31m     Erro 4422! \n \033[0;33m Responda com S ou N.\033[m')
     if ans == 'N':
         break
-print('-'*30)
+print('-'*40)
 print(f' Cod', end='')
 for i in jogador.keys():
-    print(f'{i:>6} ', end='')
+    print(f'{i:>10} ', end='')
 print()
-print(f'-'*30)
+print(f'-'*40)
 for k, v in enumerate(equipe):
     print(f' {k+1:>3}º ', end='')
     for d in v.values():
-        print(f' {str(d):<10} ', end='')
+        print(f' {str(d):<15} ', end='')
     print()
-print(f'-='*10)
+print(f'-='*20)
+while True:
+    busca = int(input(f'\033[0;32m Mostrar Dados de qual Jogador? \033[0;33m \n-Degite 999 para terminar:\033[m '))
+    if busca == 999:
+        break
+    if busca >= len(equipe):
+        print(f'\033[0;31m Erro! Player do not Exists!')
+    else:
+        print(f'\033[0;34m ---- LEVANTAMENTO DO JOGADOR {equipe[busca]["Nome"]}! ---\033[m')
+        for i, g in enumerate(equipe[busca]['Golos']):
+            print(f'No jogo {i+1}º fez {g} Golos!')
+    print(f'-='*30)
 
-
-"""print(f'O jogador {jogador["Nome"]} Jogou {len(jogador["Golos"])} jogos! ')
-for i, v in enumerate(jogador["Golos"]):
-    print(f' No {i+1}º jogo fez {v} Golos!')
-print(f'Foi um total de {jogador["Total G"]}')"""
