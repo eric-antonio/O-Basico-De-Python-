5cde0fe8bd79cc3c5365389861932c194583d8f6
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-13 15:38:39 +0200

    Create ex096_Função que calcula area.py

---
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex096_Função que calcula area.py
@@ -0,0 +1,10 @@
+def area():
+    print(f'Controle de Terrenos')
+    print('-'*20)
+    l = float(input(f' Largura (m): '))
+    c = float(input(f'Comprimento (m): '))
+    a = l * c
+    print(f' A área de terreno {l:2f} x {c:2f} é de {a}m^2')
+
+
+area()
