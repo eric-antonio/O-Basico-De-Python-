69004958973825bb9503e18902cb09dd73d484f2
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-13 12:18:19 +0200

    Update aula020_Funções em Python.py
    Fouções com parâmetro!

--- Python_Mundo123/Aulas/aula020_Funções em Python/aula020_Funções em Python.py
+++ Python_Mundo123/Aulas/aula020_Funções em Python/aula020_Funções em Python.py
@@ -1,6 +1,14 @@
 def l():
     print(f'\033[0;33m -=\033[m'*10)
-
 l()
 print(f'Curso em Video'.center(25))
 l()
+
+#Função com parametro
+msg = str(input(f'Insira o texto: '))
+def mensagem(msg):
+    print(f'\033[0;33m -=\033[m' * 10)
+    print(f'{msg}'.center(25))
+    print(f'\033[0;33m -=\033[m' * 10)
+
+mensagem(msg)
