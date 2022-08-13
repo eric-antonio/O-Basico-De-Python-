fd76e3205793653ba6629e76e7da92b6399626de
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-13 12:44:46 +0200

    Update aula020_Funções em Python.py
    Fim da aula

--- Python_Mundo123/Aulas/aula020_Funções em Python/aula020_Funções em Python.py
+++ Python_Mundo123/Aulas/aula020_Funções em Python/aula020_Funções em Python.py
@@ -1,4 +1,4 @@
-def l():
+""""def l():
     print(f'\033[0;33m -=\033[m'*10)
 l()
 print(f'Curso em Video'.center(25))
@@ -11,4 +11,36 @@ def mensagem(msg):
     print(f'{msg}'.center(25))
     print(f'\033[0;33m -=\033[m' * 10)
 
-mensagem(msg)
+mensagem(msg)"""
+
+""""---Praticando---"""
+""""---- Funçoes do Programa ----"""
+
+
+def soma(a, b):
+    som = a + b
+    print(f' A soma de A + B = {som}')
+
+
+def contador(*valoes):
+    s = 0
+    for i in valoes:
+        s += i
+    print(f'A soma dos valors {valoes} = {s}')
+
+
+
+def dobra(lst):
+    pos = 0
+    print(f'A lista inicial {lst}')
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1
+    print(f'A lista dobrada {lst}')
+
+
+""""---- Programa ----"""
+soma(4, 5)
+contador(4, 7, 7, 8)
+valores = [3, 4, 5, 6, 7]
+dobra(valores)
