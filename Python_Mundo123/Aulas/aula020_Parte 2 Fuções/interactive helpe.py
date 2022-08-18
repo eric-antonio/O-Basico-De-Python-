ff8c228af5d4c81d71c5df842034be35ef253c71
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-18 19:41:00 +0200

    Create interactive helpe.py
    Aprendendo sobre Interactive help

---
+++ Python_Mundo123/Aulas/aula020_Parte 2 Fuções/interactive helpe.py
@@ -0,0 +1,10 @@
+""""
+    É um metodo usado para obter informaçoes em python:
+    No console é so degitar help(), de seguida degiatar o comando ou simbolo
+    que logo de seguida tera acessoa a sua Doc! ou
+    No codigo é so : help(****).
+"""
+
+""""
+        Docstings- permite com que crirmos uma documentacao para o nosso codigo!
+"""
