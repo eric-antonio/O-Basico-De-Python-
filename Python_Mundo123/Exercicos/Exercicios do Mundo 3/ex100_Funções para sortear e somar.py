77e7dae94faeae7a64a656d623b2b9b8e6b32c3c
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-18 19:21:35 +0200

    Update ex100_Funções para sortear e somar.py

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex100_Funções para sortear e somar.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex100_Funções para sortear e somar.py
@@ -3,20 +3,22 @@ from time import sleep
 
 
 def sortear(lista):
+    print(f'Sorteando 5 Valores')
     for i in range(0, 4):
         n =randint(1,10)
         lista.append(n)
         print(f'{n}', end=' ')
         sleep(0.2)
+    print()
 
 
-def somaPar(li):
+def somaPar(lista):
     soma = 0
-    for i in li:
-        if i % 2 == 0:
-            li.append(i)
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
 
-    print(li)
 
 numeros = list()
 sortear(numeros)
