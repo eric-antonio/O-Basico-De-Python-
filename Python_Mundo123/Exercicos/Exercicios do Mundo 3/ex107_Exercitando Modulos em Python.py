bff27bf1969fbe32fcfa366c2a569e40fa7f9a70
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-22 21:43:46 +0200

    Comecando 107

---
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex107_Exercitando Modulos em Python.py
@@ -0,0 +1,4 @@
+
+
+num = int(input(f'Insira um valor: '))
+print(f'A metade de {num} é ')
---
+++ Python_Mundo123/Exercicos/function/__init__.py
@@ -0,0 +1,12 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumento10(n):
+    return n * 0.1
+
+
