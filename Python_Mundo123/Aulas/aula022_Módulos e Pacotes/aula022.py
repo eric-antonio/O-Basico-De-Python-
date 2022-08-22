f7b7aaa2832e4af5f1a358bd0d47c824bafd2990
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-22 14:21:39 +0200

    Update Pacotes

--- Python_Mundo123/Aulas/aula022_Módulos e Pacotes/aula022.py
+++ Python_Mundo123/Aulas/aula022_Módulos e Pacotes/aula022.py
@@ -1,10 +1,21 @@
-def fatorial(n):
-    f = 1
-    for i in range(1, n+1):
-        f *= i
-    return f
+from uteis import *
 
+"""
+      Em caso de comflito de importações ou existam modulos com 
+      mesmas funções recomenda-se a usar essa foema de importar:
+            
+            import uteis
+      
+            num = int(input(f'Degite um numero: '))
+            fat = uteis.fatorial(num)
+            print(f'O fatorial de {num}! é {fat} \n'
+                  f'O dobro do {num} é {uteis.dobro(num)}\n'
+                  f'O triplo de {num} é {uteis.triplo(num)}')
+      
+"""
 
 num = int(input(f'Degite um numero: '))
 fat = fatorial(num)
-print(f'O fatorial de {num}! é {fat} ')
+print(f'O fatorial de {num}! é {fat} \n'
+      f'O dobro do {num} é {dobro(num)}\n'
+      f'O triplo de {num} é {triplo(num)}')
