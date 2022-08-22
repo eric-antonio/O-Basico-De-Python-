921e4957ebdc982ea01f272c5f98e7d7d9d74f92
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-22 13:42:40 +0200

    Uptade 106
    Preciso rever esse código 'e um pouco complexo!

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex106_Sistema interativo de ajuda em Python.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex106_Sistema interativo de ajuda em Python.py
@@ -1,13 +1,18 @@
-
+from time import  sleep
 c = ('\033[0;31m',
      '\033[0;32m',
      '\033[0;33m',
      '\033[0;34m',
      '\033[0;35m',
-     '\033[0;36m')
+     '\033[0;36m',
+     '\033[7;30')
 
 def ajuda(com):
+    titulo(f'Acessando o manul do comando \'{com}\'', 4)
+    print(c[6], end='')
     help(com)
+    print(c[0], end='')
+    sleep(2)
 
 def titulo(msg, cor=0):
     tam = len(msg) + 4
@@ -16,13 +21,15 @@ def titulo(msg, cor=0):
     print(f' {msg}')
     print('-' * tam)
     print(c[0], end='')
+    sleep(1)
 
 
 comando = ''
 while True:
-    titulo('Sistema de Ajuda PyHelp',2 )
-    comando = str(input(f'Função ou Biblioteca > '))
+    titulo('Sistema de Ajuda PyHelp ', 2)
+    comando = str(input(f'\033[m Função ou Biblioteca > '))
     if comando.upper() == 'FIM':
         break
-    else: ajuda(comando)
+    else:
+        ajuda(comando)
     titulo('Termino da Biblioteca!', 1)
