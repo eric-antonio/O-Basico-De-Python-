323123332ecff4a4e14c654e95954a3bb44e3fb0
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-22 14:01:31 +0200

    Aula 21 Módulos e Pacote

---
+++ Python_Mundo123/Aulas/aula022_Módulos e Pacotes/aula022.py
@@ -0,0 +1,10 @@
+def fatorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+num = int(input(f'Degite um numero: '))
+fat = fatorial(num)
+print(f'O fatorial de {num}! é {fat} ')
