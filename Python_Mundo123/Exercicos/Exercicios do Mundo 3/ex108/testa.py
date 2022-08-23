178d1ba10e7becb790c563161733d908539fd180
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-23 19:58:20 +0200

    Update testa.py

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex108/testa.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex108/testa.py
@@ -4,4 +4,3 @@ print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))} \n'
       f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}\n'
       f'Aumentando 10% temos {moeda.moeda(moeda.aumentar(p, 10))}\n'
       f'Diminuindo 10% temos {moeda.moeda(moeda.diminuir(p, 10))}')
-
