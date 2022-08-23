7007ffb44dd96839b997a102bb05c600b161cebc
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-23 19:58:30 +0200

    Update testa.py

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex107/testa.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex107/testa.py
@@ -4,4 +4,3 @@ print(f'A metade de é {moeda.metade(p)} \n'
       f'O dobro de é {moeda.dobro(p)}\n'
       f'Aumentando 10% temos {moeda.aumentar(p, 10)}\n'
       f'Diminuindo 10% temos {moeda.diminuir(p, 10)}')
-
