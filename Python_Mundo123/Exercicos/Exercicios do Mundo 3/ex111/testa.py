bad7b9dfa6fe75472310a84a8a80166ee2893778
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-23 19:57:09 +0200

    Update

--- Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex111/testa.py
+++ Python_Mundo123/Exercicos/Exercicios do Mundo 3/ex111/testa.py
@@ -2,7 +2,4 @@ from utilidadescv import moeda
 p = float(input(f'Degite o preco (MT): '))
 print(moeda.resumo(p, 20, 10))
 
-""""print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)} \n'
-      f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}\n'
-      f'Aumentando 10% temos {moeda.aumentar(p, 10, True)}\n'
-      f'Diminuindo 10% temos {moeda.diminuir(p, 5, True)}')"""
+
