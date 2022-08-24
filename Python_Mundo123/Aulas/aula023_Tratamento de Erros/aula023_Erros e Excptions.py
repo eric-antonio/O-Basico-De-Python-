e15a04af00276c079262a4c6d2aad667a2e1f6bd
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-24 14:43:29 +0200

    Update aula023_Erros e Excptions.py

--- Python_Mundo123/Aulas/aula023_Tratamento de Erros/aula023_Erros e Excptions.py
+++ Python_Mundo123/Aulas/aula023_Tratamento de Erros/aula023_Erros e Excptions.py
@@ -2,9 +2,15 @@ try:
     a = int(input(f'Degite um numero:'))
     b = int(input(f'Degite um numero: '))
     r = a / b
-except:
-    print(f'Infelizmente tivemos um problema!'
-          f'Reveja o Código!')
+except (ValueError, TypeError):
+    print(f'\033[031m Tivemos um problema com os tipos de dados\033[m')
+except ZeroDivisionError:
+    print('\033[031m Não é possivel dividir por Zero!\033[m')
+except KeyboardInterrupt:
+    print(f'\033[031m O usuario não introduziu os dados!\033[m')
+except Exception as erro:
+    print(f'O erro encomtado foi {erro.__cause__}')
 else:
-    print(f'O reso;tade de {a} / {b} = {r:.1f}')
-    
+    print(f'\033[033m  {a} / {b} = {r:.1f}\033[m')
+finally:
+    print(f'Desenvolvido por: \033[0;35m https://gitHub.com/eric-antonio\033[m')
