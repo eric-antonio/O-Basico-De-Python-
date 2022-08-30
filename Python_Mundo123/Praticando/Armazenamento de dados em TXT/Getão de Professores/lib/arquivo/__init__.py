feea672195b225a6e69409c07ce320ee48faa2d3
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-30 15:41:12 +0200

    Update __init__.py

--- Python_Mundo123/Praticando/Armazenamento de dados em TXT/Getão de Professores/lib/arquivo/__init__.py
+++ Python_Mundo123/Praticando/Armazenamento de dados em TXT/Getão de Professores/lib/arquivo/__init__.py
@@ -19,3 +19,18 @@ def criarArquivo(nome):
         interface.cabeçalho(f'\033[0;31m Houve um Erro! Na crição do Arquivo.\033[m')
     else:
         interface.cabeçalho(f'\033[0;32m Arquivo! {nome} Criado com sucesso!\033[m')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        interface.cabeçalho(f'\033[0;31m ERRO AO LER O ARQUIVO\033[m'.center(30))
+    else:
+        interface.cabeçalho(f'\003[0;33m  Professores Cdastrados \033[m')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30} - {dado[1]:>2} {dado[2]:>}')
+    finally:
+        a.close
