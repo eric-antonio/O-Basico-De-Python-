863f4ca34d588be648d667e94177cf28c796a3d3
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-22 11:30:16 +0200

    Update __init__.py
    O init de aramazenamneto  foi alterado!

--- Python_Mundo123/Praticando/Armazenamento de dados em TXT/Getão de Professores/lib/arquivo/__init__.py
+++ Python_Mundo123/Praticando/Armazenamento de dados em TXT/Getão de Professores/lib/arquivo/__init__.py
@@ -21,6 +21,7 @@ def criarArquivo(nome):
         interface.cabeçalho(f'\033[0;32m Arquivo! {nome} Criado com sucesso!\033[m')
 
 
+#----Ler Arquivo-------
 def lerArquivo(nome):
     try:
         a = open(nome, 'rt')
@@ -33,4 +34,25 @@ def lerArquivo(nome):
             dado[1] = dado[1].replace('\n', '')
             print(f'{dado[0]:<30} - {dado[1]:>2} {dado[2]:>}')
     finally:
-        a.close
+        a.close()
+
+
+#-----Cdastras no arquivo-----
+def cadstrar(arq, nome='<Desconhecido>', idade=0):
+    try:
+        a = open(arq,'at')
+    except:
+        interface.cabeçalho(f'\033[0;31m Não foi possivel localizar o Arquivo de resgistro! \033[m')
+    else:
+        try:
+            a.write(f'{nome}; {idade}\n')
+        except:
+            interface.cabeçalho(f'\033[0;31m Não consegiu registar Bugou!!!!\033[m ')
+        else:
+            interface.cabeçalho(f'\033[0;32m Novo registro {nome} Adicionado! \033[m')
+            a.close()
+        finally:
+            a.close()
+
+
+
